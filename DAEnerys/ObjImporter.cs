44359388ec86b7e31fe656ca4cc05c94b3347bbc
PayDay <tv.payday@gmail.com>
2017-01-02 19:32:36 +0100

    Implemented algorithm to combine meshes with the same material on OBJ import.

--- DAEnerys/ObjImporter.cs
+++ DAEnerys/ObjImporter.cs
@@ -1,6 +1,7 @@
 ﻿using Assimp;
 using Assimp.Configs;
 using System;
+using System.Collections.Generic;
 
 namespace DAEnerys
 {
@@ -21,7 +22,48 @@ namespace DAEnerys
             Scene obj = importer.ImportFile(path, PostProcessSteps.JoinIdenticalVertices | PostProcessSteps.Triangulate | PostProcessSteps.ValidateDataStructure);
             importer.Dispose();
 
-            return obj.Meshes.ToArray();
+            //Combine meshes with the same material
+            Dictionary<Material, Mesh> materialMeshes = new Dictionary<Material, Mesh>();
+            foreach (Mesh mesh in obj.Meshes)
+            {
+                Material material = obj.Materials[mesh.MaterialIndex];
+                Mesh materialMesh = null;
+
+                if (!materialMeshes.ContainsKey(material))
+                {
+                    materialMesh = new Mesh(PrimitiveType.Triangle);
+                    materialMesh.MaterialIndex = mesh.MaterialIndex;
+                    materialMesh.Name = mesh.Name;
+                    materialMeshes.Add(material, materialMesh);
+                }
+                else
+                    materialMesh = materialMeshes[material];
+
+                List<int> indices = new List<int>();
+                if (materialMesh.FaceCount > 0)
+                    indices.AddRange(materialMesh.GetIndices());
+
+                int[] newIndices = mesh.GetIndices();
+
+                if (mesh.FaceCount > 0)
+                {
+                    for (int i = 0; i < newIndices.Length; i++)
+                        newIndices[i] += materialMesh.VertexCount;
+                    indices.AddRange(newIndices);
+                }
+
+                materialMesh.Vertices.AddRange(mesh.Vertices);
+                materialMesh.Normals.AddRange(mesh.Normals);
+                materialMesh.TextureCoordinateChannels[0].AddRange(mesh.TextureCoordinateChannels[0]);
+
+                materialMesh.SetIndices(indices.ToArray(), 3);
+            }
+
+            List<Mesh> optimizedMeshes = new List<Mesh>();
+            foreach (Mesh mesh in materialMeshes.Values)
+                optimizedMeshes.Add(mesh);
+
+            return optimizedMeshes.ToArray();
         }
 
         public static Mesh ImportMeshFromFile(string path)
